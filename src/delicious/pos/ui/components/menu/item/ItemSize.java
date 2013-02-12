d618b88598881d7b8af9dc3eb132a38d117b8aa5
Tobias Deekens <tobias.deekens@gmail.com>
2013-02-12 20:19:44 +0100

    Started wiring it all together...

--- src/delicious/pos/ui/components/menu/item/ItemSize.java
+++ src/delicious/pos/ui/components/menu/item/ItemSize.java
@@ -7,14 +7,15 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
+
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ItemSize extends UIPanel {
 
-	/**
-	 * Create the panel.
-	 */
 	public ItemSize() {
 		setLayout(new BorderLayout(0, 0));
 		
@@ -23,8 +24,21 @@ public class ItemSize extends UIPanel {
 		add(lblSize, BorderLayout.NORTH);
 		
 		JButton button = new UIButton("7.99");
+		button.setBackground(Color.gray);
 		add(button, BorderLayout.SOUTH);
-
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 	}
-
+	
+	public void select() {
+		this.setBackground(Color.green);
+	}
+	
+	public void deselect() {
+		this.setBackground(Color.gray);
+	}
 }
--- src/delicious/pos/ui/components/menu/item/ItemSizes.java
+++ src/delicious/pos/ui/components/menu/item/ItemSizes.java
@@ -1,17 +1,49 @@
 package delicious.pos.ui.components.menu.item;
 
 
+import delicious.pos.ui.components.UIButton;
 import delicious.pos.ui.components.UIPanel;
 import java.awt.FlowLayout;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ItemSizes extends UIPanel {
-
-	/**
-	 * Create the panel.
-	 */
+public class ItemSizes extends UIPanel {	
+	
+	private List<ItemSize> itemSizes = null;
+	
 	public ItemSizes() {
-		setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 5));
+		this.itemSizes = new ArrayList<ItemSize>();
+		
+		//Just to something
+		this.add(new ItemSize());
+		this.add(new ItemSize());
+		
+		UIButton button = new UIButton("-");
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		add(button);
 	}
-
+	
+	public void addItem(ItemSize itemSize) {
+		this.add(itemSize);
+	}
+	
+	public void selectSize(ItemSize itemSize) {
+		itemSize.select();
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void deselectSizes() {
+		for(ItemSize itemSize : this.itemSizes) {
+			itemSize.disable();
+		}
+	}
 }
