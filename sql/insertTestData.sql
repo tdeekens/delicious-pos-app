a3e05df9308520cf38da65879e6cd817ec94eb9a
danidanone17 <danyholzner@freenet.de>
2013-02-19 10:03:42 +0100

    Added sql scripts to setup database and fill it with test data

---
+++ sql/insertTestData.sql
@@ -0,0 +1,40 @@
+INSERT INTO Customers VALUES('Iva','Popova','Street 1','12345','City 1','123456789');
+INSERT INTO Customers VALUES('Anna','Averyanova','Street 2','12345','City 1','343456789');
+INSERT INTO Customers VALUES('Daniela','Holzner','Street 3','12346','City 2','343451189');
+
+INSERT INTO Employees VALUES('Kostas',10000,'12345678','Waiter');
+INSERT INTO Employees VALUES('John',10000,'12345678','Waiter');
+INSERT INTO Employees VALUES('Tobi',20000,'12345678','Boss');
+
+INSERT INTO Tables VALUES(1,'1');
+INSERT INTO Tables VALUES(2,'2');
+INSERT INTO Tables VALUES(3,'3');
+INSERT INTO Tables VALUES(4,'4');
+INSERT INTO Tables VALUES(5,'5');
+INSERT INTO Tables VALUES(6,'6');
+INSERT INTO Tables VALUES(7,'7');
+INSERT INTO Tables VALUES(8,'8');
+INSERT INTO Tables VALUES(9,'9');
+INSERT INTO Tables VALUES(10,'10');
+
+INSERT INTO Items VALUES('Pizza','Pizza');
+INSERT INTO Items VALUES('Gyros','Gyros');
+INSERT INTO Items VALUES('Suvlaki','Suvlaki');
+INSERT INTO Items VALUES('Bivteki','Bivteki');
+INSERT INTO Items VALUES('Feta salad','Feta salad');
+
+INSERT INTO Prices VALUES(1,60,'Small','Pizza');
+INSERT INTO Prices VALUES(2,80,'Medium','Pizza');
+INSERT INTO Prices VALUES(3,100,'Large','Pizza');
+INSERT INTO Prices VALUES(4,60,'Small','Gyros');
+INSERT INTO Prices VALUES(5,80,'Medium','Gyros');
+INSERT INTO Prices VALUES(6,100,'Large','Gyros');
+INSERT INTO Prices VALUES(7,60,'Small','Suvlaki');
+INSERT INTO Prices VALUES(8,80,'Medium','Suvlaki');
+INSERT INTO Prices VALUES(9,100,'Large','Suvlaki');
+INSERT INTO Prices VALUES(10,60,'Small','Bivteki');
+INSERT INTO Prices VALUES(11,80,'Medium','Bivteki');
+INSERT INTO Prices VALUES(12,100,'Large','Bivteki');
+INSERT INTO Prices VALUES(13,60,'Small','Feta salad');
+INSERT INTO Prices VALUES(14,80,'Medium','Feta salad');
+INSERT INTO Prices VALUES(15,100,'Large','Feta salad');
